8edbfeda9dce84b2b5a98cf6689899568c6aa421
HaileyPong <haileypong@berkeley.edu>
2023-08-26 18:27:47 -0400

    connected contact form and email

--- components/ContactSection.js
+++ components/ContactSection.js
@@ -6,7 +6,8 @@ const ContactSection = () => {
     <section className={styles.contact}>
       <h2>Contact Us</h2>
       <p>Have questions or want to know more? Reach out to us!</p>
-      <form>
+      
+      <form action="https://formsubmit.io/send/uluwatulab@gmail.com" method="POST">
         <label htmlFor="name">Name</label>
         <input type="text" id="name" name="name" required />
 
@@ -15,7 +16,7 @@ const ContactSection = () => {
 
         <label htmlFor="message">Message</label>
         <textarea id="message" name="message" rows="4" required></textarea>
-
+        
         <button type="submit">Send Message</button>
       </form>
     </section>
