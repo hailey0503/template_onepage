3fd94ff406349272d0b446736b3559235a0760ec
HaileyPong <haileypong@berkeley.edu>
2023-10-02 01:11:37 -0400

    added spacing on blog

--- pages/about.js
+++ pages/about.js
@@ -11,75 +11,63 @@ const AboutPage = () => {
       <div className={styles.container}>
         <section className={styles.about}>
           <h2>About Us</h2>
-          <p>
-            <strong>
-              Pioneering the Metaverse: Redefining Industries through AI-Driven
-              Innovation
-            </strong>
-          </p>
-        <br ></br>
-          <p>
-            <strong>Introduction</strong>
-          </p>
+          <div className={styles.intro}>
+            <h3>
+              <strong>
+                Pioneering the Metaverse: Redefining Industries through
+                AI-Driven Innovation
+              </strong>
+            </h3>
+          </div>
+          <div className={styles.paragraph}>
+            <p>
+              <strong>Introduction</strong>
+            </p>
+            <p>
+              In today's dynamic business landscape, our company is leading the
+              way in harnessing the power of AI, image training, and
+              cross-industry collaboration to revolutionize various sectors. As
+              the metaverse takes center stage, our visionary approach sets us
+              apart.
+            </p>
+          </div>
+          <div className={styles.paragraph}>
+            <p>
+              <strong>Embracing the Metaverse Revolution</strong>
+            </p>
+            <p>
+              The metaverse, a fusion of AR, VR, and digital realms, has
+              captured our imagination. Our AI-driven image training is at the
+              forefront, creating lifelike avatars and immersive environments,
+              enhancing the metaverse's authenticity.
+            </p>
+          </div>
+          {/* Add more sections with appropriate class names */}
+          <div className={styles.paragraph}>
+            <p>
+              <strong>AI-Powered Image Training</strong>
+            </p>
 
-          <p>
-            In today's dynamic business landscape, our company is leading the
-            way in harnessing the power of AI, image training, and
-            cross-industry collaboration to revolutionize various sectors. As
-            the metaverse takes center stage, our visionary approach sets us
-            apart.
-          </p>
+            <p>
+              Our expertise in AI-powered image training is redefining the
+              metaverse. From realistic avatars to breathtaking virtual
+              landscapes, we're transforming digital experiences with
+              cutting-edge algorithms and deep learning.
+            </p>
+          </div>
+          <div className={styles.paragraph}>
+            <p>
+              <strong>Collaboration Across Industries</strong>
+            </p>
 
-          <p>
-            <strong>Embracing the Metaverse Revolution</strong>
-          </p>
-
-          <p>
-            The metaverse, a fusion of AR, VR, and digital realms, has captured
-            our imagination. Our AI-driven image training is at the forefront,
-            creating lifelike avatars and immersive environments, enhancing the
-            metaverse's authenticity.
-          </p>
-
-          <p>
-            <strong>AI-Powered Image Training</strong>
-          </p>
-
-          <p>
-            Our expertise in AI-powered image training is redefining the
-            metaverse. From realistic avatars to breathtaking virtual
-            landscapes, we're transforming digital experiences with cutting-edge
-            algorithms and deep learning.
-          </p>
-
-          <p>
-            <strong>Collaboration Across Industries</strong>
-          </p>
-
-          <p>
-            We understand that innovation thrives on collaboration. By
-            partnering across various sectors, including healthcare, education,
-            and entertainment, we're creating groundbreaking solutions such as
-            medical simulations, immersive learning, and AI-driven
-            entertainment.
-          </p>
-
-          <p>
-            <strong>Reinventing Industries</strong>
-          </p>
-
-          <p>
-            Our influence extends beyond the metaverse. In healthcare, we're
-            improving training and patient care. In education, we're
-            transforming learning experiences. In entertainment, we're crafting
-            unforgettable stories with AI.
-          </p>
-
-          <p>
-            Join us on this exhilarating journey into the metaverse, where
-            imagination knows no bounds, and together, we'll shape a brighter,
-            more immersive future.
-          </p>
+            <p>
+              We understand that innovation thrives on collaboration. By
+              partnering across various sectors, including healthcare,
+              education, and entertainment, we're creating groundbreaking
+              solutions such as medical simulations, immersive learning, and
+              AI-driven entertainment.
+            </p>
+          </div>
         </section>
       </div>
       <Footer />
